5934e711f61ec7aa9b10510124355376ecfca8ab
ungvert <pavel.chelyapin@gmail.com>
2022-07-03 13:50:41 +0300

    feat: test for users module

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,8 +1,10 @@
+import { gql } from "apollo-server-express";
 import { DocumentNode, print } from "graphql";
 import request from "supertest";
 import { createServer } from "./server.js";
 
 let cachedServer: any;
+export let cachedJwt: any;
 
 export const sendTestRequest = async (
   query: DocumentNode,
@@ -26,4 +28,63 @@ export const sendTestRequest = async (
     query: print(query),
   });
   return JSON.parse(text);
+};
+
+export const testUser = {
+  firstName: "first name",
+  lastName: "last name",
+  password: "118649qwe",
+  email: "met9129@gmail.com",
+  id: undefined,
+};
+
+export const registerTestUser = async (): Promise<any> => {
+  const response = await sendTestRequest(
+    gql`
+      mutation Register(
+        $password: String!
+        $email: String!
+        $firstName: String
+        $lastName: String
+      ) {
+        register(
+          password: $password
+          email: $email
+          firstName: $firstName
+          lastName: $lastName
+        ) {
+          firstName
+          lastName
+          password
+          email
+          id
+        }
+      }
+    `,
+    {
+      variables: testUser,
+    }
+  );
+
+  testUser.id = response.data?.register?.id;
+  return response;
+};
+
+export const loginTestUser = async (): Promise<any> => {
+  const response = await sendTestRequest(
+    gql`
+      query Jwt($email: String!, $password: String!) {
+        jwt(email: $email, password: $password)
+      }
+    `,
+    {
+      variables: {
+        password: testUser.password,
+        email: testUser.email,
+      },
+    }
+  );
+
+  cachedJwt = response.data?.jwt;
+  return response;
 };
